6c4bbb4200340a4f907e8baf49bbfc695ea8b78b
guangxij <guangxij@gmail.com>
2015-10-03 13:51:31 -0700

    j

---
+++ Assets/navigate1.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navigate1 : MonoBehaviour 
+
+{
+	Transform target;               // Reference to the player's position.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	public float Time = 100f;            // How long between each spawn.
+	
+	
+	
+	
+	void Awake ()
+	{
+		// Set up the references.
+		target = GameObject.FindGameObjectWithTag ("building1").transform;
+		
+		nav = GetComponent <NavMeshAgent> ();
+		nav.SetDestination (target.position);
+	}
+	void Update ()
+	{
+		
+		// ... set the destination of the nav mesh agent to the player.
+		nav.SetDestination (target.position);
+		InvokeRepeating ("Change", Time, Time);
+		
+	} 
+	void Change()
+	{
+		target = GameObject.FindGameObjectWithTag ("dead").transform;
+		
+		nav = GetComponent <NavMeshAgent> ();
+		nav.SetDestination (target.position);
+	}
+	
+	
+	
+	//
+	
+	
+	
+	
+	
+}
---
+++ Assets/npcborn/navigate3.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navigate3 : MonoBehaviour {
+
+	Transform target;               // Reference to the player's position.
+	NavMeshAgent nav;               // Reference to the nav mesh agent.
+	public float Time = 50f;            // How long between each spawn.
+	
+	
+	
+	
+	void Awake ()
+	{
+		// Set up the references.
+		target = GameObject.FindGameObjectWithTag ("building2").transform;
+		
+		nav = GetComponent <NavMeshAgent> ();
+		nav.SetDestination (target.position);
+	}
+	void Update ()
+	{
+		
+		// ... set the destination of the nav mesh agent to the player.
+		nav.SetDestination (target.position);
+		InvokeRepeating ("Change", Time, Time);
+		
+	} 
+	void Change()
+	{
+		target = GameObject.FindGameObjectWithTag ("dead").transform;
+		
+		nav = GetComponent <NavMeshAgent> ();
+		nav.SetDestination (target.position);
+	}
+	
+	
+	
+	//
+	
+	
+	
+	
+	
+}
